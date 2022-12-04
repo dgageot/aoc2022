1fd0781174b921c3c0776055efc34f4b69b95696
David Gageot <david.gageot@docker.com>
2022-12-04 10:01:04 +0100

    Day 4
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 04/first.rb
+++ 04/first.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
-# Expected: NaN
+# Expected: 2
 
-puts File.readlines(ARGV[0]).count
+puts File
+    .foreach(ARGV[0])
+    .map { |l| l.split(/[,-]/).map(&:to_i) }
+    .count { |g|
+        l = g[0]..g[1]
+        r = g[2]..g[3]
+        l.cover?(r) || r.cover?(l)
+    }
