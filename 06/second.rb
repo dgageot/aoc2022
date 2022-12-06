2e1ab0484323a261d81d99c34f8dbf12f6dc7598
David Gageot <david.gageot@docker.com>
2022-12-06 09:13:48 +0100

    One liner
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 06/second.rb
+++ 06/second.rb
@@ -3,5 +3,8 @@
 
 len = 14
 
-msg = File.read(ARGV[0]).chomp.chars
-puts len + msg.size.times.select { |i| msg[i, len].uniq.count == len }.first
+File
+    .read(ARGV[0])
+    .chomp
+    .chars
+    .tap { |msg| puts len + msg.size.times.find { |i| msg[i, len].uniq.count == len }}
