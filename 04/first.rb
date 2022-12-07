9c1367035628416daf93a4769e93e5072d74c127
David Gageot <david.gageot@docker.com>
2022-12-07 19:01:06 +0100

    Don't use scanf
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 04/first.rb
+++ 04/first.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/env ruby
 # Expected: 2
 
-require "../common.rb"
-
 p STDIN
     .readlines(chomp: true)
-    .map { |l| l.scanf("%d-%d,%d-%d") }
-    .count { |g|
-        l = g[0]..g[1]
-        r = g[2]..g[3]
+    .map { |l| l.scan /\d+/ }
+    .count { |ls, le, rs, re|
+        l = ls..le
+        r = rs..re
         l.cover?(r) || r.cover?(l)
     }
