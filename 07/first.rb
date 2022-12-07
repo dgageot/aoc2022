017482a133d52bfb9258e1fa65a19f78615efea6
David Gageot <david.gageot@docker.com>
2022-12-07 17:23:18 +0100

    do end
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 07/first.rb
+++ 07/first.rb
@@ -7,7 +7,7 @@ total = Hash.new(0)
 File
     .readlines(ARGV[0])
     .map(&:chomp)
-    .each { |cmd|
+    .each do |cmd|
         case cmd
         when "$ cd .."
             pwd.pop
@@ -15,11 +15,11 @@ File
             pwd.push($1)
         when /(\d*) .*/
             length = $1.to_i
-            pwd.size.times.each { |i|
+            pwd.size.times.each do |i|
                 dir = pwd[0..i]
                 total[dir] += length
-            }
+            end
         end
-    }
+    end
 
 p total.values.select { |v| v <= 100000 }.sum
--- 07/second.rb
+++ 07/second.rb
@@ -7,7 +7,7 @@ total = Hash.new(0)
 File
     .readlines(ARGV[0])
     .map(&:chomp)
-    .each { |cmd|
+    .each do |cmd|
         case cmd
         when "$ cd .."
             pwd.pop
@@ -15,12 +15,12 @@ File
             pwd.push($1)
         when /(\d*) .*/
             length = $1.to_i
-            pwd.size.times.each { |i|
+            pwd.size.times.each do |i|
                 dir = pwd[0..i]
                 total[dir] += length
-            }
+            end
         end
-    }
+    end
 
 required = total[["/"]] - 40000000
 puts total.values.select { |v| v >= required }.sort.first
