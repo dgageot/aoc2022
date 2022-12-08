38b85d13cd842a149af6feb26d6064a5837d5aed
David Gageot <david.gageot@docker.com>
2022-12-08 07:56:45 +0100

    Day 8
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 08/first.rb
+++ 08/first.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/env ruby
-# Expected: NaN
+# Expected: 21
 
-# require "../common.rb"
-
-p STDIN
+grid = STDIN
     .readlines(chomp: true)
-    .count
+    .map(&:chars)
+
+transposed = grid.transpose
+
+count = 0
+
+grid.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+        left = row[0...x]
+        right = row[x+1...]
+        up = transposed[x][0...y]
+        down = transposed[x][y+1...]
+
+        seen = ->(neighbours) { neighbours.none? { |t| t >= tree} }
+        count +=1 if seen[left] || seen[right] || seen[up] || seen[down]
+    end
+end
+
+p count
--- 08/second.rb
+++ 08/second.rb
@@ -1,8 +1,29 @@
 #!/usr/bin/env ruby
-# Expected: NaN
+# Expected: 8
 
-# require "../common.rb"
-
-p STDIN
+grid = STDIN
     .readlines(chomp: true)
-    .count
+    .map(&:chars)
+
+transposed = grid.transpose
+
+max = 0
+
+grid.each_with_index do |row, y|
+    row.each_with_index do |tree, x|
+        left = row[0...x]
+        right = row[x+1...]
+        up = transposed[x][0...y]
+        down = transposed[x][y+1...]
+
+        scenic = ->(neighbours) {
+            highest = neighbours.index { |t| t >= tree }
+            highest ? highest + 1 : neighbours.size
+        }
+
+        score = scenic[up.reverse] * scenic[down] * scenic[left.reverse] * scenic[right]
+        max = [max, score].max
+    end
+end
+
+p max
