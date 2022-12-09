9a3eaf305818b1ad8e654337ccf71ead2604eb86
David Gageot <david.gageot@docker.com>
2022-12-09 09:23:29 +0100

    Simpler code
    Signed-off-by: David Gageot <david.gageot@docker.com>

--- 09/first.rb
+++ 09/first.rb
@@ -3,44 +3,42 @@
 
 require "../common.rb"
 
+class XY
+    attr_reader :x, :y
+
+    def initialize(x, y) @x, @y = x, y end
+
+    def move(h, v) XY.new(x + h, y + v) end
+
+    def dist(t) (x - t.x) ** 2 + (y - t.y) ** 2 end
+
+    def nearest(ref)
+        return self if dist(ref) <= 2
+        [-1, 0, +1]
+            .product([-1, 0, +1])
+            .map { |x, y| move(x, y) }
+            .min_by { |xy| xy.dist(ref) }
+    end
+end
+
 delta = {
     "R" => [+1, +0],
     "L" => [-1, +0],
     "U" => [+0, -1],
     "D" => [+0, +1],
 }
-move = ->(xy, direction) { xy.zip(delta[direction]).map(&:sum) }
-nearest = ->(xy, ref) {
-    [
-        xy.zip([-1, -1]).map(&:sum),
-        xy.zip([-1, +0]).map(&:sum),
-        xy.zip([-1, +1]).map(&:sum),
-        xy.zip([+0, -1]).map(&:sum),
-        xy.zip([+0, +0]).map(&:sum),
-        xy.zip([+0, +1]).map(&:sum),
-        xy.zip([+1, -1]).map(&:sum),
-        xy.zip([+1, +0]).map(&:sum),
-        xy.zip([+1, +1]).map(&:sum),
-    ].min_by { |xy| (xy[0] - ref[0]).abs + (xy[1] - ref[1]).abs }
-}
 
-h = [0, 0]
-t = [0, 0]
-seen = [t]
+knots = [XY.new(0, 0)] * 2
+seen = Set[[knots.last.x, knots.last.y]]
 
 STDIN
     .readlines(chomp: true)
     .map { |line| line.scanf("%s %d") }
-    .each { |direction, step|
-        step.times.each { |i|
-            h = move[h, direction]
-
-            xdiff = (t[0] - h[0]).abs
-            ydiff = (t[1] - h[1]).abs
-            t = nearest[t, h] if xdiff > 1 || ydiff > 1
-
-            seen.push(t)
-        }
+    .flat_map { |direction, step| [delta[direction]] * step }
+    .map { |∆|
+        knots[0] = knots[0].move(*∆)
+        (knots.size - 1).times { |i| knots[i+1] = knots[i+1].nearest(knots[i]) }
+        seen.add([knots.last.x, knots.last.y])
     }
 
-puts seen.uniq.count
+puts seen.count
--- 09/second.rb
+++ 09/second.rb
@@ -3,45 +3,42 @@
 
 require "../common.rb"
 
+class XY
+    attr_reader :x, :y
+
+    def initialize(x, y) @x, @y = x, y end
+
+    def move(h, v) XY.new(x + h, y + v) end
+
+    def dist(t) (x - t.x) ** 2 + (y - t.y) ** 2 end
+
+    def nearest(ref)
+        return self if dist(ref) <= 2
+        [-1, 0, +1]
+            .product([-1, 0, +1])
+            .map { |x, y| move(x, y) }
+            .min_by { |xy| xy.dist(ref) }
+    end
+end
+
 delta = {
     "R" => [+1, +0],
     "L" => [-1, +0],
     "U" => [+0, -1],
     "D" => [+0, +1],
 }
-move = ->(xy, direction) { xy.zip(delta[direction]).map(&:sum) }
-nearest = ->(xy, ref) {
-    [
-        xy.zip([-1, -1]).map(&:sum),
-        xy.zip([-1, +0]).map(&:sum),
-        xy.zip([-1, +1]).map(&:sum),
-        xy.zip([+0, -1]).map(&:sum),
-        xy.zip([+0, +0]).map(&:sum),
-        xy.zip([+0, +1]).map(&:sum),
-        xy.zip([+1, -1]).map(&:sum),
-        xy.zip([+1, +0]).map(&:sum),
-        xy.zip([+1, +1]).map(&:sum),
-    ].min_by { |xy| (xy[0] - ref[0]).abs + (xy[1] - ref[1]).abs }
-}
 
-coord = [[0, 0]] * 10
-seen = [coord.last]
+knots = [XY.new(0, 0)] * 10
+seen = Set[[knots.last.x, knots.last.y]]
 
 STDIN
     .readlines(chomp: true)
     .map { |line| line.scanf("%s %d") }
-    .each { |direction, step|
-        step.times.each { |i|
-            coord[0] = move[coord[0], direction]
-
-            (coord.size - 1).times { |i|
-                xdiff = (coord[i+1][0] - coord[i][0]).abs
-                ydiff = (coord[i+1][1] - coord[i][1]).abs
-                coord[i+1] = nearest[coord[i+1], coord[i]] if xdiff > 1 || ydiff > 1
-            }
-
-            seen.push(coord.last)
-        }
+    .flat_map { |direction, step| [delta[direction]] * step }
+    .map { |∆|
+        knots[0] = knots[0].move(*∆)
+        (knots.size - 1).times { |i| knots[i+1] = knots[i+1].nearest(knots[i]) }
+        seen.add([knots.last.x, knots.last.y])
     }
 
-puts seen.uniq.count
+puts seen.count
