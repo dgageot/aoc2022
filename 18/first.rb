140717ca7f4213a9c952f28e96502f564c19fa77
David Gageot <david.gageot@docker.com>
2022-12-17 23:41:53 +0100

    Day 18
    Signed-off-by: David Gageot <david.gageot@docker.com>

---
+++ 18/first.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# Expected: 64
+
+Cube = Struct.new(:x, :y, :z)
+
+cubes = STDIN
+    .readlines(chomp: true)
+    .map { |line| line.split(",").map(&:to_i) }
+    .map { |x, y, z| Cube.new(x, y, z) }
+
+space = cubes.to_h { |c| [[c.x, c.y, c.z], 1] }
+
+p cubes.map { |c|
+    6 - [
+        [+1, +0, +0],
+        [-1, +0, +0],
+        [+0, +1, +0],
+        [+0, -1, +0],
+        [+0, +0, +1],
+        [+0, +0, -1],
+    ].count { |x, y, z| space[[c.x + x, c.y + y, c.z + z]] == 1 }
+}.sum
---
+++ 18/second.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+# Expected: 58
+
+Cube = Struct.new(:x, :y, :z)
+
+cubes = STDIN
+    .readlines(chomp: true)
+    .map { |line| line.split(",").map(&:to_i) }
+    .map { |x, y, z| Cube.new(x, y, z) }
+
+space = cubes.to_h { |c| [[c.x, c.y, c.z], "█"] }
+
+cubes.each do |c|
+    [[+1, +0, +0], [-1, +0, +0], [+0, +1, +0], [+0, -1, +0], [+0, +0, +1], [+0, +0, -1]].each do |x, y, z|
+        coord = [c.x + x, c.y + y, c.z + z]
+        next if space[coord] == "█"
+        space[coord] ||= 0
+        space[coord] += 1
+    end
+end
+
+class Tester
+    def initialize(space)
+        @space = space
+        @min_x, @max_x = space.keys.map { |x, _, _| x }.minmax
+        @min_y, @max_y = space.keys.map { |_, y, _| y }.minmax
+        @min_z, @max_z = space.keys.map { |_, _, z| z }.minmax
+    end
+
+    def test
+        total = @space.values.filter { |v| v != "█" }.sum
+        flood
+        trapped = @space.values.filter { |v| v != "█" && v != "W" }.sum
+        total - trapped
+    end
+
+    private
+
+    def flood
+        @space[[@min_x, @min_y, @min_z]] = "W"
+        loop do
+            break if flood_once == 0
+        end
+    end
+
+    def flood_once
+        changed = 0
+
+        (@min_x..@max_x).each do |x|
+            (@min_y..@max_y).each do |y|
+                (@min_z..@max_z).each do |z|
+                    next if @space[[x, y, z]] == "█" || @space[[x, y, z]] == "W"
+
+                    flooded = [[+1, +0, +0], [-1, +0, +0], [+0, +1, +0], [+0, -1, +0], [+0, +0, +1], [+0, +0, -1]].any? { |dx, dy, dz|
+                        @space[[x + dx, y + dy, z + dz]] == "W"
+                    }
+                    if flooded then
+                        @space[[x, y, z]] = "W" 
+                        changed += 1
+                        next
+                    end
+                end
+            end
+        end
+        changed
+    end
+end
+
+p Tester.new(space).test
